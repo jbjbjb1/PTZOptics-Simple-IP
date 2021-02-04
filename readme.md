50b202fa0ea9a9dd0abb64e41cac3862c680a9bd
jbjbjb1 <johnpbills@gmail.com>
2021-02-04 14:57:55 +0800

    first version of zoom level

--- readme.md
+++ readme.md
@@ -7,6 +7,7 @@ Not ready for use, currently in development.
 ## Background
 * zoom-in-fully.py -> is working (based off below link) 
 * Python examples from PTZ: https://help.ptzoptics.com/support/solutions/articles/13000077734-an-introduction-to-ip-control-scripting-for-ptzoptics-cameras)
+* The user manual for the camera: https://ptzoptics.com/wp-content/uploads/2019/07/PT12X-ZCAM-User-Manual-v1_2-rev-7-19.pdf
 * A Github project that has advanced features but I can't get working: https://github.com/misterhay/VISCA-IP-Controller
 
 ## How to test (Ubuntu)
---
+++ zoom-command.py
@@ -0,0 +1,42 @@
+# This script asks the user what level zoom to do.
+
+import socket
+
+# Camera settings
+IPADDR1 = '192.168.8.99'
+PORTNUM = 1259
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+s.connect((IPADDR1, PORTNUM))
+
+# Commands
+COMMAND1 = '8101040702FF'       # zooms all the way in
+
+def COMMAND2(zoom_code):
+    ''' Zooms to a pre-defined locations. '''
+    command_template = '81010447pqrsFF'
+    p = zoom_code[0]
+    q = zoom_code[1]
+    r = zoom_code[2]
+    s = zoom_code[3]
+    command = command_template.replace('p', p)
+    command = command.replace('q', q)
+    command = command.replace('q', r)
+    command = command.replace('q', s)
+
+    return command
+
+# Command line to call and send commands
+while True:
+    # Range for p, q, r , s is: 0000 to 1770 (for 20X zoom)
+    # Above info from: https://ptzoptics.com/wp-content/uploads/2014/09/PTZOptics-VISCA-Commands-r1.pdf
+    zoom = float(input('What is the desired zoome level (0 to 20)?'))
+    zoom_val = 1770 * (zoom/20)
+    zoom_code = str(int(zoom_val)).zfill(4)
+    print(f'Zoom code for {zoom} of zoom val {zoom_val} is {zoom_code} will be sent.')
+
+    data = bytes.fromhex(COMMAND2(zoom_code))
+
+    #s.send(data)
+
+    s.close()
